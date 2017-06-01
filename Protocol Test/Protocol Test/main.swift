009d902c97a62a5712b0175cbc8c41e07cc798c0
zohrath <zohrath@gmail.com>
2017-06-01 23:04:34 +0200

    Protocol testing

---
+++ Protocol Test/Protocol Test/main.swift
@@ -0,0 +1,75 @@
+import Foundation
+
+protocol Bird {
+    var name: String { get }
+    var canFly: Bool { get }
+}
+
+extension Bird {
+ 
+    var canFly: Bool { return self is Flyable }
+}
+
+protocol Flyable {
+    var airspeedVelocity: Double { get }
+}
+
+struct FlappyBird: Bird, Flyable {
+    let name: String
+    let flappyAmplitude: Double
+    let flappyFrequency: Double
+    let canFly = true
+    
+    var airspeedVelocity: Double {
+        return 3 * flappyFrequency * flappyAmplitude
+    }
+}
+
+struct Penguin: Bird {
+    let name: String
+    let canFly = false
+}
+
+struct SwiftBird: Bird, Flyable {
+    var name: String { return "Swift \(version)" }
+    let version: Double
+    let canFly = true
+    
+    // Swift is FASTER every version!
+    var airspeedVelocity: Double { return version * 1000.0 }
+}
+
+enum UnladenSwallow: Bird, Flyable {
+    case african
+    case european
+    case unknown
+    
+    var name: String {
+        switch self {
+        case .african:
+            return "African"
+        case .european:
+            return "European"
+        case .unknown:
+            return "What do you mean? African or European?"
+        }
+    }
+    
+    var airspeedVelocity: Double {
+        switch self {
+        case .african:
+            return 10.0
+        case .european:
+            return 9.9
+        case .unknown:
+            fatalError("You are thrown from the bridge of death!")
+        }
+    }
+}
+
+extension UnladenSwallow {
+    var canFly: Bool {
+        return self != .unknown
+    }
+}
+
